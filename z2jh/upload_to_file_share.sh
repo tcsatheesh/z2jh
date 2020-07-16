7044445ebaf342da46a6097d38c42eeed078232e
Satheesh TC <tc.satheesh@microsoft.com>
2020-07-16 11:34:57 +0100

    really complicated way to automatically redirect

--- z2jh/upload_to_file_share.sh
+++ z2jh/upload_to_file_share.sh
@@ -11,8 +11,39 @@ SAS_TOKEN=$(az storage share generate-sas \
 
 DESTINATION_URL=https://$STORAGE_ACCOUNT_NAME.file.core.windows.net/$FILE_SHARE_NAME/?$SAS_TOKEN
 
-./azcopy_linux_amd64_10.4.3/azcopy copy 'notebooks/*' $DESTINATION_URL --recursive
+./azcopy_linux_amd64_10.5.0/azcopy copy 'notebooks/*' $DESTINATION_URL --recursive
 
-az storage share list \
---account-key $STORAGE_ACCOUNT_KEY \
---account-name $STORAGE_ACCOUNT_NAME 
+# az storage share list \
+# --account-key $STORAGE_ACCOUNT_KEY \
+# --account-name $STORAGE_ACCOUNT_NAME 
+
+FILE_SHARE_NAME=uitemplates
+SAS_TOKEN_END_DATE=$(date -u -d "30 minutes" '+%Y-%m-%dT%H:%MZ')
+SAS_TOKEN=$(az storage share generate-sas \
+      --name $FILE_SHARE_NAME \
+      --account-name $STORAGE_ACCOUNT_NAME \
+      --account-key $STORAGE_ACCOUNT_KEY \
+      --https-only \
+      --permissions dlrw \
+      --expiry $SAS_TOKEN_END_DATE \
+      -o tsv)
+
+DESTINATION_URL=https://$STORAGE_ACCOUNT_NAME.file.core.windows.net/$FILE_SHARE_NAME/?$SAS_TOKEN
+
+./azcopy_linux_amd64_10.5.0/azcopy copy 'uitemplates/*' $DESTINATION_URL --recursive
+
+FILE_SHARE_NAME=modules
+SAS_TOKEN_END_DATE=$(date -u -d "30 minutes" '+%Y-%m-%dT%H:%MZ')
+SAS_TOKEN=$(az storage share generate-sas \
+      --name $FILE_SHARE_NAME \
+      --account-name $STORAGE_ACCOUNT_NAME \
+      --account-key $STORAGE_ACCOUNT_KEY \
+      --https-only \
+      --permissions dlrw \
+      --expiry $SAS_TOKEN_END_DATE \
+      -o tsv)
+
+DESTINATION_URL=https://$STORAGE_ACCOUNT_NAME.file.core.windows.net/$FILE_SHARE_NAME/?$SAS_TOKEN
+
+./azcopy_linux_amd64_10.5.0/azcopy copy 'lmod/modules/*' $DESTINATION_URL --recursive
+
